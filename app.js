d4069d2013239da8b17d6dec5b89e929914b5b9b
muriel <murielthoms@gmail.com>
2016-10-10 23:18:01 +0200

    projet todolist commit initial

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 alert("Bienvenue sur votre to do list");
 console.log('hello');
 
-function ajoutTache() {
-	var ajoutNote = $("input").html();
-	
-}
-
-	$('button').click(function() {
-
-});
 
 		
