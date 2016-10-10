1678a39a7b942a7223d6a6f0a7c9685f9a67e69d
muriel <murielthoms@gmail.com>
2016-10-10 17:03:48 +0200

    debut projet todolist

---
+++ app.js
@@ -0,0 +1,13 @@
+alert("Bienvenue sur votre to do list");
+console.log('hello');
+
+function ajoutTache() {
+	var ajoutNote = $("input").html();
+	
+}
+
+	$('button').click(function() {
+
+});
+
+		
