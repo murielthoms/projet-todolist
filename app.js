7d84c77803c3179682e2b38d623bbbdb6819228c
muriel <murielthoms@gmail.com>
2016-10-11 13:47:19 +0200

    ajout de la liste

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-alert("Bienvenue sur votre to do list");
-console.log('hello');
+//alert("Bienvenue sur votre to do list");
+// console.log('hello');
 
 
-		
+// var todo = [];
+// console.log(todo);
+$(document).ready();
+
+
+
+$("button").click(function() {
+	var  recup = $('input').val();
+	$('ul').append('<li>' + recup + '</li>');
+	//console.log(recup);
+	//console.log(list);
+	
+});
+
